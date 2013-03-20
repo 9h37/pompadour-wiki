ec135099b048a49ceaae54f7722da0c5de97891c
David Delassus <david.jose.delassus@gmail.com>
2013-03-20 16:42:22 +0100

    add unittest for tag system

--- pompadour_wiki/pompadour_wiki/apps/tagging/tests.py
+++ pompadour_wiki/pompadour_wiki/apps/tagging/tests.py
@@ -1,16 +1,45 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+# -*- coding: utf-8 -*-
 
 from django.test import TestCase
 
+from pompadour_wiki.apps.tagging.models import Tag
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TagTest(TestCase):
+    def test_00_nothing(self):
+        """ Make sure there is no objects """
+
+        c = Tag.objects.count()
+
+        self.assertEqual(c, 0)
+
+    def test_01_create(self):
+        """ Create a tag """
+
+        # Create
+        t = Tag()
+        t.page = 'test/Test'
+        t.tag = 'test'
+        t.save()
+
+        # Make sure it's here
+        c = Tag.objects.count()
+        self.assertEqual(c, 1)
+
+        # Make sure it's the good one
+        tags = Tag.objects.filter(page='test/Test')
+
+        self.assertNotEqual(tags, Tag.objects.none())
+        self.assertEqual(tags.count(), 1)
+        self.assertEqual(tags[0].tag, 'test')
+        self.assertEqual(tags[0].page, 'test/Test')
+
+    def test_02_delete(self):
+        """ Delete a tag """
+        self.test_01_create()
+
+        tag = Tag.objects.all()[0]
+
+        tag.delete()
+
+        c = Tag.objects.count()
+        self.assertEqual(c, 0)
